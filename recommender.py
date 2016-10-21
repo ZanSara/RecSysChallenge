ee7ae26d8d61e2d82fe8557afd4c8dfd583fe760
ZanSara <chinasaz@yandex.com>
2016-10-21 18:29:43 +0200

    Script refinements Now writing automatically into the .csv file

--- recommender.py
+++ recommender.py
@@ -4,16 +4,15 @@
 import numpy as np
 import scipy as sp
 
-from scipy.stats import itemfreq
-
 # Utility imports
 from optparse import OptionParser
+import csv
 
 # Constants
 TRAINING_SET = "full_datasets/"
 TESTING_LOCAL_SET = "full_datasets/"
 TESTING_FULL_SET = "full_datasets/"
-RESULT = "result.csv"
+SUBMISSION = "submission.csv"
 
 
 
@@ -35,38 +34,47 @@ else:
     test_set = TESTING_LOCAL_SET
     print(" Local dataset chosen for testing")
     
-    
-# Loading data
+
+###### LOADING DATASETS #####
+
+# Interactions' structure
+# user_id	item_id		interaction_type	created_at
 print("\t# Loading interactions dataset...")
-interactions_map = sp.genfromtxt("{}interactions.csv".format(test_set), delimiter="\t")
+interactions_map = sp.genfromtxt("{}interactions.csv".format(test_set), dtype='int64', delimiter="\t")
 print("\t  -> got {} rows".format(interactions_map.shape[0]))
+
+# Item Profiles' structure
+# id	title	career_level	discipline_id	industry_id	country	region	latitude	longitude	employment	tags	created_at	active_during_test
 print("\t# Loading item profiles' dataset...")
-item_profiles = sp.genfromtxt("{}item_profile.csv".format(test_set), delimiter="\t")
+item_profiles = sp.genfromtxt("{}item_profile.csv".format(test_set), dtype='int64', delimiter="\t")
 print("\t  -> got {} rows".format(item_profiles.shape[0]))
-#print("\t# Loading user profiles' dataset...")
-#user_profiles = sp.genfromtxt("{}user_profile.csv".format(test_set), delimiter="\t")
-#print("\t  -> got {} rows".format(user_profiles.shape[0]))
-#print("\t# Loading target users' dataset...")
-#target_users = sp.genfromtxt("{}target_users.csv".format(test_set), delimiter="\t")
-#print("\t  -> got {} rows".format(target_users.shape[0]))
+
+# User Profiles' structure
+# user_id	jobroles	career_level	discipline_id	industry_id	country	region	experience_n_entries_class	experience_years_experience	experience_years_in_current	edu_degree	edu_fieldofstudies
+print("\t# Loading user profiles' dataset...")
+user_profiles = sp.genfromtxt("{}user_profile.csv".format(test_set), dtype='int64', delimiter="\t")
+print("\t  -> got {} rows".format(user_profiles.shape[0]))
+
+# Target users' structure
+# user_id
+print("\t# Loading target users' dataset...")
+target_users = sp.genfromtxt("{}target_users.csv".format(test_set), dtype='int64', delimiter="\t")
+print("\t  -> got {} rows".format(target_users.shape[0]))
 print("\t# Datasets loaded.")
 
 
 
-##### First try with a TopPop Algorithm ######
+#########  ALGORITHM - TopPop  #########
 
 item_codes = item_profiles[1:, 0]
 
 # Selects the row of interacting items and the row of ratings
 interacting_items = interactions_map[1:,1:3]
-print(interacting_items[:5])
 
 # Sort table!
 print(" # Sorting results...")
 interacting_items = np.sort(interacting_items, axis=0)
 print(" # Sort completed!")
-print("--------------")
-print(interacting_items[:5])
 
 # Count each block and store the top five frequencies
 top_five = [[0, 0], [0,0], [0,0], [0,0], [0,0]]
@@ -74,7 +82,6 @@ buffer_item = [0, 0]
 count = 0
 
 for i in range(interacting_items.shape[0]):
-	#print(buffer_item[0])
 	if interacting_items[i][0] == buffer_item[0]:
 		buffer_item[1] = buffer_item[1] + interacting_items[i][1]
 	else:
@@ -86,50 +93,24 @@ for i in range(interacting_items.shape[0]):
 		buffer_item[1] = 1
 	buffer_item[0] = interacting_items[i][0]
 
+recommendations = np.array(top_five)
 print(top_five)
 
 
 
-# Create the submission file
-print("\t# Loading submissions' file template...")
-submission = sp.genfromtxt("sample_submission.csv", delimiter=",")
-print("\t  -> got it!".format(item_profiles.shape[0]))
+
+###### WRITE SUBMISSION FILE ###################
 
 recommendations = ""
 for rec in top_five:
-	recommendations += "{} ".format(rec[0])
+	recommendations += "{} ".format(rec[0])	
 	
-#for row in submission:
-#	row[1] = recommendations
+with open(SUBMISSION, 'wb') as csvfile:
+	csvfile.write(bytes("user_id,recommended_items\n", 'UTF-8'))	
+	row = []
+	for i in range(1, target_users.shape[0]):
+		csvfile.write(bytes("{},{}\n".format(target_users[i], recommendations), 'UTF-8'))
 
-print(recommendations)
-#np.savetxt("submission.csv", submission, delimiter=",")
+print("{} wrote successfully. Bye!".format(SUBMISSION))
 
-
-
-
-
-
-##### Try with the Global Effects Algorithm ######
-
-# Extract useful data
-#interacting_users = interactions_map[:,0]
-#interacting_users = interacting_users[~sp.isnan(interacting_users)]
-#interacting_items = interactions_map[:,1]
-#interacting_items = interacting_items[~sp.isnan(interacting_items)]
-#interactions_values = interactions_map[:,2]
-#interactions_values = interactions_values[~sp.isnan(interactions_values)]
-
-# Average Rating (Mu)
-#sum_of_interactions = sp.sum(interactions_values)
-#num_of_valid_interactions = interactions_values.shape[0] - sp.sum(sp.isnan(interactions_values))
-#mu = np.divide(sum_of_interactions, num_of_valid_interactions)
-#print(" # Computing MU: Sum is {}, num is {} -----> mu seems to be {}".format(sum_of_interactions, num_of_valid_interactions, mu))
-
-# Item Biases (Bi)
-
-#item_ratings_matrix = np.matrix() # gonna have this structure: item - sum_ratings - n_ratings
-
-#item_rating_matrix[:,0] = interacting_items
-#item_rating_matrix[:,1] = 
 
